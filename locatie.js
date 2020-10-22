9b9cf4af16e8cf55c5bb7fd3f014303fead0aec3
Motoraa <72560939+Motoraa@users.noreply.github.com>
2020-10-22 17:12:44 +0300

    Add files via upload

--- locatie.js
+++ locatie.js
@@ -8,8 +8,7 @@ function on_success(position)
 	var latlon=position.coords.latitude+ "," +position.coords.longitude;
 	
 	
-	var img_url= "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+
-	"&zoom=14&size=400x300&key=AIzaSyDIni3W4Lj1K6n9aKppUSGC1mBIWxPQCig";
+	var img_url= "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyDIni3W4Lj1K6n9aKppUSGC1mBIWxPQCig";
 	
 	document.getElementById("map").innerHTML= "<img src='"+img_url+"'>";
 }
@@ -21,7 +20,7 @@ function on_success(position)
 		enableHighAccuracy:true;
 		timeout:1000,
 		maximumAge:0
-		);
+	};
 		
 	funciton get_geo_postion()
 	{
